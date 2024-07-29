97cabde157f5d4a1609e5b09adda83563f904e1b
jiaxiaoyu <xyyy1420@gmail.com>
2024-07-29 16:41:41 +0800

    support using FzfLua search result from global tags
    Signed-off-by: jiaxiaoyu <xyyy1420@gmail.com>

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -141,11 +141,8 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       local fzf_lua = require("fzf-lua")
-
-      local function global_update()
-        local handle = io.popen("global -u")
-        handle:close()
-      end
+      local builtin_previewer = require("fzf-lua.previewer.builtin")
+      local global_previewer = builtin_previewer.buffer_or_file:extend()
 
       local function split(str,reps)
         local resultStrList = {}
@@ -153,6 +150,24 @@ return {
           table.insert(resultStrList,w)
         end)
         return resultStrList
+      end
+
+      function global_previewer:new(o, opts, fzf_win)
+        global_previewer.super.new(self, o, opts, fzf_win)
+        setmetatable(self, global_previewer)
+        return self
+      end
+
+      function global_previewer:parse_entry(entry_str)
+        local selected_split = split(entry_str, " ")
+        local file_path = selected_split[3]
+        local file_lines = selected_split[2]
+        return {path = file_path, line = file_lines, col = 1}
+      end
+
+      local function global_update()
+        local handle = io.popen("global -u")
+        handle:close()
       end
 
       local function create_user_command(user_command, shell_command, nargs)
@@ -166,23 +181,32 @@ return {
                 ['default'] = function (selected, o)
                   local selected_split = split(selected[1], " ")
                   local file = fzf_lua.path.entry_to_file(selected_split[3], o)
-                  print(file.path)
                   local cmd = string.format("edit %s", file.path)
                   vim.cmd(cmd)
                 end
-              }
+              },
+              previewer = global_previewer,
             })
           end,
           {
-            nargs = nargs
+            nargs = nargs,
+            complete = function (opts)
+              local branches = vim.fn.systemlist(shell_command .. " -c " .. opts)
+              if vim.v.shell_error == 0 then
+                return branches
+              end
+            end
           }
         )
       end
 
+      create_user_command("GlobalTagsInFilename", "global -x -P ", 1)
+      -- not support search tags in files
+      -- create_user_command("GlobalTagsInFiles", "global -x -f ", 1)
       create_user_command("GlobalTags", "global -x ", 1)
-      create_user_command("GlobalTagsInFilename", "global -x -f ", 1)
-      create_user_command("GlobalGrep", "global -x -g ", 1)
-      create_user_command("GlobalTagsInFiles", "global -x -f ", 1)
+      create_user_command("GlobalGrep", "global -x -E ", 1)
+      create_user_command("GlobalReference", "global -x -r ", 1)
+      create_user_command("GlobalDefinition", "global -x -d ", 1)
 
       vim.api.nvim_create_user_command('GlobalUpdate', function()
         global_update()
